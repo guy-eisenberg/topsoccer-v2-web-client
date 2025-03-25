b58df054217a8ca1290cb08952fb1f7dcc55e1c1
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-03-25 21:41:13 +0200

    Upgrade maintenance page

--- app/(empty)/maintenance/page.tsx
+++ app/(empty)/maintenance/page.tsx
@@ -7,9 +7,13 @@ export default async function MaintenancePage() {
   if (!maintenance) redirect("/");
 
   return (
-    <main className="flex h-full w-full flex-col items-center justify-center px-4 py-2 text-4xl font-semibold md:p-0">
+    <main className="flex h-full w-full flex-col items-center justify-center px-4 py-2 font-semibold md:p-0">
       <Logo className="absolute top-12 w-64 animate-bounce" />
-      <h1>אנחנו כרגע בשיפוצים, נחזור בהקדם :)</h1>
+      <h1 className="mb-4 text-4xl">אנחנו כרגע בשיפוצים, נחזור בהקדם :)</h1>
+      <pre className="rounded-xl border border-theme-green bg-theme-green/10 px-3 py-2 text-center">
+        חמים לקבוע משחק אבל לא רוצים להמתין?
+        <pre /> דברו איתנו בווטסאפ: 0549733105
+      </pre>
     </main>
   );
 }

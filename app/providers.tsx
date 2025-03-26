35987e8e2f7e3b8b6e5e29091744d5573d1c6b83
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-03-26 11:25:09 +0200

    Default theme is now system

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider defaultTheme="dark" attribute="class">
+    <ThemeProvider attribute="class">
       <HeroUIProvider className="flex flex-1 flex-col md:h-full">
         <ToastProvider placement="top-center" />
         <RouterProvider>{children}</RouterProvider>

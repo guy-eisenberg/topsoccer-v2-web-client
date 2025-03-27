27f330ae3bcd6031d460b8a5b934dc8db9fc462e
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-03-27 19:41:50 +0200

    /players-stats page layout fixes

--- app/(dashboard)/players-stats/page.tsx
+++ app/(dashboard)/players-stats/page.tsx
@@ -11,29 +11,27 @@ export default async function PlayersStatsPage({
   const { trunc } = await searchParams;
 
   return (
-    <main className="flex-1">
-      <div className="flex flex-col gap-2">
-        <div className="flex items-center justify-between">
-          <p className="whitespace-nowrap text-xl font-semibold">
-            סטטיסטיקות שחקנים:
-          </p>
-          <QuerySelect
-            className="max-w-36"
-            size="sm"
-            field="trunc"
-            fallback="all"
-            options={[
-              { key: "all", label: "מצטבר" },
-              { key: "month", label: "החודש" },
-              { key: "week", label: "השבוע" },
-            ]}
-            disallowEmptySelection
-          />
-        </div>
-        <Suspense fallback={<PlayersStatsTableLoading />} key={trunc}>
-          <PlayersStatsTable trunc={trunc || "all"} />
-        </Suspense>
+    <main className="mt-2 flex flex-1 flex-col gap-2">
+      <div className="flex items-center justify-between">
+        <p className="whitespace-nowrap text-xl font-semibold">
+          סטטיסטיקות שחקנים:
+        </p>
+        <QuerySelect
+          className="max-w-36"
+          size="sm"
+          field="trunc"
+          fallback="all"
+          options={[
+            { key: "all", label: "מצטבר" },
+            { key: "month", label: "החודש" },
+            { key: "week", label: "השבוע" },
+          ]}
+          disallowEmptySelection
+        />
       </div>
+      <Suspense fallback={<PlayersStatsTableLoading />} key={trunc}>
+        <PlayersStatsTable trunc={trunc || "all"} />
+      </Suspense>
     </main>
   );
 }

8a3ba2efb2c3f15bfc5178496e1fc4b74ab23ca6
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-03-27 19:49:46 +0200

    Fix DatePicker background color in dark mode on focus

--- app/components/core/DatePicker.tsx
+++ app/components/core/DatePicker.tsx
@@ -9,7 +9,7 @@ const _DatePicker = extendVariants(__DatePicker, {
     classNames: {
       input: "text-base",
       inputWrapper:
-        "border border-theme-light-gray hover:bg-white focus-within:hover:bg-white shadow-none bg-white dark:bg-default-100 group-data-[focus=true]:bg-white dark:group-data-[focus=true]:bg-default-100 dark:data-[hover=true]:bg-default-100 data-[hover=true]:bg-white group-data-[focus=true]:border-theme-green hover:border-theme-green/75",
+        "border border-theme-light-gray dark:focus-within:hover:bg-default-100 focus-within:hover:bg-white shadow-none bg-white dark:bg-default-100 group-data-[focus=true]:bg-white dark:group-data-[focus=true]:bg-default-100 dark:data-[hover=true]:bg-default-100 data-[hover=true]:bg-white group-data-[focus=true]:border-theme-green hover:border-theme-green/75",
     } as any,
   },
 });

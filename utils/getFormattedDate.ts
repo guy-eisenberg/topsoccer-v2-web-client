bbe3bec884d39c054fdcc6b311e3274f8315d0b2
Guy Eisenberg <guyeisenberg@guyeisenberg.com>
2025-03-30 18:29:50 +0300

    Using @internationalized/date for getFormattedDate

--- utils/getFormattedDate.ts
+++ utils/getFormattedDate.ts
@@ -1,14 +1,16 @@
-export function getFormattedDate(ms: number) {
-  const date = new Date(ms);
+import { fromAbsolute } from "@internationalized/date";
 
-  const year = date.getFullYear();
-  const _month = date.getMonth() + 1;
+export function getFormattedDate(ms: number) {
+  const date = fromAbsolute(ms, "Asia/Jerusalem");
+
+  const year = date.year;
+  const _month = date.month;
   const month = `${_month < 10 ? "0" : ""}${_month}`;
-  const _day = date.getDate();
+  const _day = date.day;
   const day = `${_day < 10 ? "0" : ""}${_day}`;
-  const _hour = date.getHours();
+  const _hour = date.hour;
   const hour = `${_hour < 10 ? "0" : ""}${_hour}`;
-  const _minute = date.getMinutes();
+  const _minute = date.minute;
   const minute = `${_minute < 10 ? "0" : ""}${_minute}`;
 
   return { year, month, day, hour, minute };
